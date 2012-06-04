f70f38ebbe23c3da6cc482a0dd2190a6c893a1c3
Maxim Dikun <dikmax@gmail.com>
2012-06-04 17:51:59 +0300

    Show right count of comments according to russian language

--- static/js/script.js
+++ static/js/script.js
@@ -16,4 +16,20 @@ $(function() {
 		document.location = '/post/' + url;
 		return false;
 	});
+	
+	$('p.post-comments > a').bind("DOMCharacterDataModified", function (e) {
+		var text = e.target.innerText;
+		var match = text.match(/^(\d+) комментариев/);
+		if (match) {
+			var count = Number(match[1]);
+			if (count % 100 != 1) {
+				var count10 = count % 10;
+				if (count10 == 1) {
+					e.target.innerText = count + ' комментарий';
+				} else if (count10 >= 2 && count10 <= 4) {
+					e.target.innerText = count + ' комментария';
+				}
+			}
+		}
+	});
 });
