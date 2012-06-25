4c5b0850b55c3633f928028f0a56f6a1a01beaba
Maxim Dikun <dikmax@gmail.com>
2012-06-25 21:04:47 +0300

    Extract vault module\nShoutbox

--- src/Site.hs
+++ src/Site.hs
@@ -5,7 +5,6 @@ module Site
   ) where
 
 ------------------------------------------------------------------------------
-import           Blaze.ByteString.Builder (toByteString)
 import           Control.Applicative
 import           Control.Monad.Trans
 import           Data.ByteString (ByteString)
@@ -19,51 +18,23 @@ import           Database.HDBC.MySQL
 import           Prelude hiding (id)
 import           Snap.Core
 import           Snap.Snaplet
-import           Snap.Snaplet.Heist
 import           Snap.Snaplet.Hdbc
+import           Snap.Snaplet.Heist
 import           Snap.Snaplet.Session
 import           Snap.Snaplet.Session.Backends.CookieSession
 import           Snap.Util.FileServe
-import           System.Locale
-import           Text.Pandoc
-import           Text.Pandoc.Highlighting
 import           Text.Templating.Heist
 import           Text.XmlHtml hiding (render)
 ------------------------------------------------------------------------------
 import           Application
 import           Config
 import           Database
+import           Site.Common
 import           Site.Rss
+import           Site.Vault
 import           Site.Vault.Files
 import           Site.Utils
 import           Types
-
-timeLocale :: TimeLocale
-timeLocale = defaultTimeLocale 
-  { wDays =
-    [ ("Воскресенье", "вс")
-    , ("Понедельник", "пн")
-    , ("Вторник", "вт")
-    , ("Среда", "ср")
-    , ("Четверг", "чт")
-    , ("Пятница", "пт")
-    , ("Суббота", "сб")
-    ]
-  , months = 
-    [ ("января", "янв")
-    , ("февраля", "фев")
-    , ("марта", "мар")
-    , ("апреля", "апр")
-    , ("мая", "май")
-    , ("июня", "июн")
-    , ("июля", "июл")
-    , ("августа", "авг")
-    , ("сентября", "сен")
-    , ("октября", "окт")
-    , ("ноября", "ноя")
-    , ("декабря", "дек")
-    ]  
-  }
 
 ------------------------------------------------------------------------------
 index :: Handler App App ()
@@ -130,54 +101,6 @@ renderPostInList post =
     ]
   ]
 
-renderSinglePost :: Post -> Node 
-renderSinglePost post = 
-  Element "div" [("class", "post")] [
-    Element "p" [("class", "post-date")] 
-      [TextNode $ T.pack $ formatTime timeLocale "%A, %e %B %Y, %R." $ 
-        postDate post],
-    Element "h1" [("class", "post-title")] 
-      [TextNode $ postTitle post],
-    renderPostBody post
-  ]
-
--- TODO create my own writer (instead of writeHml) with blackjack and hookers  
-renderPostBody :: Post -> Node
-renderPostBody post =
-  Element "div" [("class", "post-body")] $
-    either (\ a -> [TextNode $ T.pack a]) extractData
-  (parseHTML "post" $
-     T.encodeUtf8 $
-       T.pack $
-         writeHtmlString writerOptions $
-           readMarkdown parserState $ T.unpack $ postText post)
-  ++ renderTags (postTags post)
-  where
-    extractData (HtmlDocument _ _ content) = content
-    extractData (XmlDocument _ _ content) = content       
-
-renderTags :: [Text] -> [Node]
-renderTags [] = []
-renderTags tags = [Element "div" [("class", "post-tags")] $ renderTags' tags]
-  where
-    renderTags' (t:[]) = [Element "a" [("href", "/tag/" `T.append` t)]
-      [TextNode t]]
-    renderTags' (t:ts) = [Element "a" [("href", "/tag/" `T.append` t)]
-      [TextNode t], TextNode ", "] ++ renderTags' ts
-    renderTags' _ = []
-
-parserState :: ParserState
-parserState = defaultParserState 
-  { stateSmart = True
-  , stateParseRaw = True
-  }
-  
-writerOptions :: WriterOptions
-writerOptions = defaultWriterOptions
-  { writerHighlight = True,
-    writerHighlightStyle = kate
-  }
-
 -- |
 -- Show post Action
 --
@@ -209,187 +132,18 @@ aboutSplice = do
   return $ maybe [] (\p -> [renderPostBody p]) post
 
 -- |
--- Vault action
+-- About me action
 --
-vault :: AppHandler ()
-vault = do
-  isAdminLogin <- with sessLens $ getFromSession "isAdminLogin"
-  maybe (render "vaultlogin") (\_ -> vaultMain) isAdminLogin
-
-vaultMain :: AppHandler ()
-vaultMain = heistLocal (bindSplice "posts" vaultPostsListSplice) $
-  render "vault"
-
-vaultPostsListSplice :: Splice AppHandler
-vaultPostsListSplice = do
-  posts <- lift vaultGetPostsList
-  return $ map renderPost posts
-  where 
-    renderPost post = Element "tr" 
-      [ ("data-rowid", T.pack $ show $ postId post)
-      , ("data-url", postUrl post) ] 
-      [ Element "td" [] [TextNode $ T.pack $ show $ postDate post]
-      , Element "td" [] [TextNode $ if postPublished post then "+" else ""]
-      , Element "td" [] 
-        [ TextNode $ postTitle post
-        , Element "div" [] $ renderTags $ postTags post
-        ]
-      , Element "td" [("class", "actions")] 
-        [ Element "span" [("class", "action-view")] []
-        , Element "span" [("class", "action-delete")] []
-        ]
-      ]
-
-vaultEdit :: AppHandler ()
-vaultEdit = do
-  request <- getRequest
-  id <- decodedParam "id"
-  post <- getPost' id
-  case rqMethod request of
-    POST -> vaultSave
-    _ -> heistLocal (bindSplice "vault-form" $ vaultPostForm post) $ 
-      render "vaultedit"
-  where
-    getPost' :: HasHdbc m c s => ByteString -> m Post
-    getPost' "" = liftIO newPost
-    getPost' id = getPostById id
-    
--- TODO there should be a way to simplify this function
-vaultGetPost :: AppHandler Post
-vaultGetPost = do
-  id <- decodedPostParam "id"
-  title <- decodedPostParam "title"
-  text <- decodedPostParam "text"
-  url <- decodedPostParam "url"
-  date <- decodedPostParam "date"
-  published <- decodedPostParam "published"
-  special <- decodedPostParam "special"
-  tags <- decodedPostParam "tags"  
-  return Post 
-      { postId = read $ unpack id -- TODO validate
-      , postTitle = T.decodeUtf8 title
-      , postText = T.replace "\r\n" "\n" $ T.decodeUtf8 text
-      , postDate = read $ unpack date -- TODO check for format errors
-      , postUrl = T.decodeUtf8 url -- TODO check for duplicate
-      , postPublished = published /= ""
-      , postSpecial = special /= ""
-      , postTags = stringToTags $ T.decodeUtf8 tags
-      }
-
-
-vaultSave :: AppHandler ()
-vaultSave = do
-  post <- vaultGetPost
-  savePost post
-  redirect "/vault"
-
--- TODO digestive functors
-vaultPostForm :: Post -> Splice AppHandler
-vaultPostForm (Post id title text url date published special tags) =
-  return 
-    [ Element "form" 
-      [ ("class", "form-horizontal post-form")
-      , ("method", "post")
-      ] 
-      [ Element "input" 
-        [ ("type", "hidden")
-        , ("name", "id")
-        , ("value", T.pack $ show id)
-        ] []
-      , Element "fieldset" [] 
-        [ Element "legend" [] [TextNode "Редактирование записи"]
-        , inputText "Заголовок" "title" title
-        , inputText "Url" "url" url
-        , inputText "Дата" "date" $ T.pack $ show date
-        , inputCheckbox "Опубликовано" "published" published
-        , inputCheckbox "Специальный" "special" special
-        , textarea "Текст" "text" text
-        , inputText "Теги" "tags" $ tagsToString tags
-        , Element "div" [("class", "form-actions")] 
-          [ Element "button" 
-            [ ("type", "submit")
-            , ("class", "btn btn-primary")
-            ] 
-            [TextNode "Сохранить"]
-          , Element "button" [("class", "btn")] [TextNode "Отмена"]
-          , Element "button" 
-            [ ("class", "btn btn-refresh") ] 
-            [ TextNode "Обновить" ]
-          ]
-        ]
-      ]
-    ]
-  where
-    inputText :: Text -> Text -> Text -> Node
-    inputText fieldLabel name value = field fieldLabel name [
-        Element "input" [("type", "text"), ("name", name), 
-          ("class", "input-fullwidth"), ("id", "post-" `T.append` name), 
-          ("value", value)] []
-      ]
-    inputCheckbox :: Text -> Text -> Bool -> Node
-    inputCheckbox fieldLabel name value = field fieldLabel name [
-        Element "input" 
-          ([("type", "checkbox"), ("name", name), 
-            ("id", "post-" `T.append` name)] ++ 
-              [("checked", "checked") | value])  
-          []
-      ]
-    textarea :: Text -> Text -> Text -> Node
-    textarea fieldLabel name value = field fieldLabel name [
-        Element "textarea" [("name", name),  
-          ("id", "post-" `T.append` name),
-          ("class", "input-fullwidth monospace"), ("rows", "20")] 
-          [TextNode value]
-      ]
-    field :: Text -> Text -> [Node] -> Node
-    field fieldLabel fieldName fieldControl =
-      Element "div" [("class", "control-group")] [
-        Element "label" [("class", "control-label"), 
-          ("for", "post-" `T.append` fieldName)] [
-          TextNode fieldLabel
-        ],
-        Element "div" [("class", "controls")] fieldControl
-      ]
-
-vaultRenderPost :: AppHandler ()
-vaultRenderPost = do
-  post <- vaultGetPost 
-  writeBS $ toByteString $ renderHtmlFragment UTF8 [renderSinglePost post]
+shoutbox :: AppHandler ()
+shoutbox = heistLocal (bindSplices
+  [ ("shoutbox", shoutboxSplice)
+  , ("page-title", pageTitleSplice $ Just "Shoutbox")
+  ] ) $ render "shoutbox"
   
-vaultAction :: AppHandler ()
-vaultAction = do
-  action <- decodedPostParam "action"
-  case action of
-    "login" -> do
-      login <- decodedPostParam "login"
-      password <- decodedPostParam "password"
-      if (login == adminLogin) && (password == adminPassword)
-        then do
-          with sessLens $ do 
-            setInSession "isAdminLogin" "1"
-            commitSession
-          redirect "/vault"
-        else heistLocal (bindString "error" "Неверные данные") $ 
-          render "vaultlogin"
-
-    "logout" -> do
-      with sessLens $ do
-        deleteFromSession "isAdminLogin"
-        commitSession
-      redirect "/vault"
-
-    _ -> redirect "/vault"
-
-vaultDelete :: AppHandler ()
-vaultDelete = do
-  id <- decodedParam "id"
-  deletePost id
-  redirect "/vault"
-
-vaultAllowed :: AppHandler () -> AppHandler ()
-vaultAllowed action = do 
-  isAdminLogin <- with sessLens $ getFromSession "isAdminLogin"
-  maybe (redirect "/vault") (\_ -> action) isAdminLogin
+shoutboxSplice :: Splice AppHandler
+shoutboxSplice = do
+  post <- lift $ getPost "shoutbox"
+  return $ maybe [] (\p -> [renderPostBody p]) post
   
 --
 -- Navigation
@@ -397,6 +151,7 @@ vaultAllowed action = do
 siteStructure :: [(String, String)]
 siteStructure = 
   [ ("/about", "Обо мне")
+  , ("/shoutbox", "Shoutbox")
   ]
 
 createList :: String -> [Node]
@@ -446,6 +201,7 @@ routes =
   , ("/tag/:tag/page/:page", index)
   , ("/post/:post", showPost)
   , ("/about", aboutMe)
+  , ("/shoutbox", shoutbox)
   , ("/rss", rss)
   , ("/vault", method POST vaultAction)
   , ("/vault", vault)
---
+++ src/Site/Common.hs
@@ -0,0 +1,89 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Site.Common where
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import Data.Time
+import System.Locale
+import Text.XmlHtml hiding (render)
+import Text.Pandoc
+import Text.Pandoc.Highlighting
+
+import Types
+
+timeLocale :: TimeLocale
+timeLocale = defaultTimeLocale 
+  { wDays =
+    [ ("Воскресенье", "вс")
+    , ("Понедельник", "пн")
+    , ("Вторник", "вт")
+    , ("Среда", "ср")
+    , ("Четверг", "чт")
+    , ("Пятница", "пт")
+    , ("Суббота", "сб")
+    ]
+  , months = 
+    [ ("января", "янв")
+    , ("февраля", "фев")
+    , ("марта", "мар")
+    , ("апреля", "апр")
+    , ("мая", "май")
+    , ("июня", "июн")
+    , ("июля", "июл")
+    , ("августа", "авг")
+    , ("сентября", "сен")
+    , ("октября", "окт")
+    , ("ноября", "ноя")
+    , ("декабря", "дек")
+    ]  
+  }
+
+renderTags :: [Text] -> [Node]
+renderTags [] = []
+renderTags tags = [Element "div" [("class", "post-tags")] $ renderTags' tags]
+  where
+    renderTags' (t:[]) = [Element "a" [("href", "/tag/" `T.append` t)]
+      [TextNode t]]
+    renderTags' (t:ts) = [Element "a" [("href", "/tag/" `T.append` t)]
+      [TextNode t], TextNode ", "] ++ renderTags' ts
+    renderTags' _ = []
+
+renderSinglePost :: Post -> Node 
+renderSinglePost post = 
+  Element "div" [("class", "post")] [
+    Element "p" [("class", "post-date")] 
+      [TextNode $ T.pack $ formatTime timeLocale "%A, %e %B %Y, %R." $ 
+        postDate post],
+    Element "h1" [("class", "post-title")] 
+      [TextNode $ postTitle post],
+    renderPostBody post
+  ]
+
+-- TODO create my own writer (instead of writeHml) with blackjack and hookers  
+renderPostBody :: Post -> Node
+renderPostBody post =
+  Element "div" [("class", "post-body")] $
+    either (\ a -> [TextNode $ T.pack a]) extractData
+  (parseHTML "post" $
+     T.encodeUtf8 $
+       T.pack $
+         writeHtmlString writerOptions $
+           readMarkdown parserState $ T.unpack $ postText post)
+  ++ renderTags (postTags post)
+  where
+    extractData (HtmlDocument _ _ content) = content
+    extractData (XmlDocument _ _ content) = content       
+
+parserState :: ParserState
+parserState = defaultParserState 
+  { stateSmart = True
+  , stateParseRaw = True
+  }
+  
+writerOptions :: WriterOptions
+writerOptions = defaultWriterOptions
+  { writerHighlight = True,
+    writerHighlightStyle = kate
+  }
