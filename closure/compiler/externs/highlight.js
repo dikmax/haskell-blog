2f630b4348af504203646ae409db571c839765c1
Maxim Dikun <dikmax@gmail.com>
2012-09-19 22:18:24 +0300

    Closure compiler

---
+++ closure/compiler/externs/highlight.js
@@ -0,0 +1,12 @@
+var hljs = {
+    /**
+     * @type {Object}
+     */
+    LANGUAGES: '',
+    highlight:  function(language_name, value) {},
+    highlightAuto: function(text) {},    
+    fixMarkup: function(value, tabReplace, useBR) {},
+    highlightBlock: function (block, tabReplace, useBR) {},
+    initHighlighting: function () {},
+    initHighlightingOnLoad: function () {}
+};
