0fc2adc58893c576dffef978ad1c8424e69635b3
Maxim Dikun <dikmax@gmail.com>
2013-01-10 17:29:27 +0300

    Modify splice for use blaze-html

--- src/Site/Front/Splices.hs
+++ src/Site/Front/Splices.hs
@@ -6,57 +6,34 @@ import           Data.Text (Text)
 import qualified Data.Text as T
 import           Data.Time
 import           Heist.Compiled
-import           Text.XmlHtml
+import           Text.Blaze.Renderer.Utf8
+import qualified Text.Blaze.XHtml5 as H
+import           Text.Blaze.XHtml5
+import qualified Text.Blaze.XHtml5.Attributes as A
+import           Text.Blaze.XHtml5.Attributes
 
 import           Application
-import qualified HtmlTags as H
-import           HtmlTags ((<@), (<.), (<#), (<&), (<&&))
-import qualified HtmlAttributes as A
 
 
 -- | Splice to show years in copyright
 copyrightYearSplice :: Integer -> Splice AppHandler
 copyrightYearSplice startYear = do
-    return $ yieldRuntime $ do
-        currentTime <- liftIO getCurrentTime
-        return $ renderHtmlFragment UTF8
-           [
-              H.span <@ A.itemprop "copyrightYear" <#
-                (
-                  if (startYear == getYear currentTime)
-                    then
-                      T.pack (show startYear)
-                    else
-                      T.pack (show startYear) `T.append`
-                      " — " `T.append`
-                      T.pack (show $ getYear currentTime)
-                )
-           ]
-    where
-      getYear time = getYear_ $ toGregorian $ localDay $ utcToLocalTime (minutesToTimeZone 180) time
-      getYear_ (year, _, _) = year
+  return $ yieldRuntime $ do
+    currentTime <- liftIO getCurrentTime
+    return $ renderMarkupBuilder $
+      H.span ! itemprop "copyrightYear" $
+        if (startYear == getYear currentTime)
+          then
+            toMarkup startYear
+          else do
+            toMarkup startYear
+            " — "
+            toMarkup $ getYear currentTime
+  where
+    getYear time = getYear_ $ toGregorian $ localDay $ utcToLocalTime (minutesToTimeZone 180) time
+    getYear_ (year, _, _) = year
 
---copyrightYearSplice startYear = do
---  currentTime <- liftIO getCurrentTime
---  return
---    [
---      H.span <@ A.itemprop "copyrightYear" <#
---        (
---          if (startYear == getYear currentTime)
---            then
---              T.pack (show startYear)
---            else
---              T.pack (show startYear) `T.append`
---              " — " `T.append`
---              T.pack (show $ getYear currentTime)
---        )
---    ]
---  where
---    getYear time = getYear_ $ toGregorian $ localDay $ utcToLocalTime (minutesToTimeZone 180) time
---    getYear_ (year, _, _) = year
-
--- [(T.Text, SnapletISplice App)]
 compiledSplices :: [(Text, Splice AppHandler)]
 compiledSplices =
-    [ ("copyrightYear", copyrightYearSplice 2012)
-    ]
+  [ ("copyrightYear", copyrightYearSplice 2012)
+  ]
