08040b3103c6c099173ac0841f786128bde2459f
Maxim Dikun <dikmax@gmail.com>
2013-04-17 23:12:13 +0300

    Working on dispatcher

--- src/Site/Front/Dispatcher.hs
+++ src/Site/Front/Dispatcher.hs
@@ -3,10 +3,16 @@ module Site.Front.Dispatcher where
 
 ------------------------------------------------------------------------------
 import           Data.ByteString.Char8 (pack)
+import qualified Data.HashMap.Strict as H
+import           Data.Maybe
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 import           Snap.Core
 import           Snap.Snaplet.Heist
+
 ------------------------------------------------------------------------------
 import           Application
+import           Site.Common.Config
 import           Site.Snaplet.CommonData
 import           Site.Snaplet.DbCache
 import           Site.Types
@@ -17,15 +23,26 @@ import           Site.Types
 dispatcher :: AppHandler ()
 dispatcher = do
   blog <- getData cdBlog
-  navigation <- getNavigation blog
-  writeBS $ pack $ show navigation
-  {-if isCommonBlog blog || isCompoundBlog blog
-    then dispatchBlog $ blogId blog
+  -- writeBS $ pack $ show navigation
+  if not $ isUnknownBlog blog
+    then dispatchBlog blog
     else redirect defaultDomain
 
   where
-    dispatchBlog blogId = do
+    dispatchBlog blog = do
+      navigation <- getNavigation blog
       request <- getRequest
-      let path = rqPathInfo request-}
+      let path = getPath $ T.decodeUtf8 $ rqPathInfo request
+
+      maybe
+        (writeBS "Not found")
+        (\p -> writeBS $ pack $ show $ blogNavigationPostId p) $
+        H.lookup path navigation
+
+
+    getPath path
+      | "/" `T.isSuffixOf` path = T.init path
+      | otherwise = path
+
       -- TODO drop last / if present
       -- "/" `S.isSuffixOf` uri
