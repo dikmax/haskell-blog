901aac12a4afa2b4e2445f81c07772aaa44f63c9
Maxim Dikun <dikmax@gmail.com>
2013-07-03 10:24:18 +0300

    Add math rendering

--- static/js/dikmax/App.js
+++ static/js/dikmax/App.js
@@ -528,6 +528,12 @@ dikmax.App.prototype.renderVaultPreview_ = function(opt_formData) {
       goog.dom.getElementByClass('render-area').innerHTML =
           request.getResponseText();
       me.highlightBlocks_();
+      setTimeout(function() {
+        window['MathJax']['Hub']['Queue']([
+            'Typeset', window['MathJax']['Hub']
+        ]);
+      }, 100);
+
     }
   }, 'POST', opt_formData);
 };
