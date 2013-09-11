4f86f39745faac79666461209fa2953652f42b03
Maxim Dikun <dikmax@gmail.com>
2013-09-11 08:10:44 +0300

    New pagination

--- site.hs
+++ site.hs
@@ -1,14 +1,14 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Control.Monad (forM_, zipWithM_, liftM, filterM)
-import           Data.List (sortBy, intercalate)
+import           Control.Monad (forM_, filterM)
+import           Data.List (sortBy, intercalate, unfoldr)
+import qualified Data.Map as M
 import           Data.Monoid (mappend)
-import           Data.Time.Clock (UTCTime)
-import           Data.Time.Format (parseTime)
+-- import           Data.Time.Clock (UTCTime)
+-- import           Data.Time.Format (parseTime)
 import           Hakyll
-import           Hakyll.Web.Tags
-import           System.FilePath (takeBaseName, takeFileName, replaceFileName, replaceExtension)
-import           System.Locale (defaultTimeLocale)
+-- import           System.FilePath (takeBaseName, takeFileName, replaceFileName, replaceExtension)
+-- import           System.Locale (defaultTimeLocale)
 import           Text.Regex (mkRegex, subRegex)
 
 
@@ -25,20 +25,16 @@ main = hakyll $ do
 
     tags <- buildTags "posts/*" (\tag -> fromFilePath $ "tag/" ++ tag ++ "/index.html")
 
-    {-match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls-}
+    -- Posts pages
 
-    matchPosts $ \identifier ->
-        create [identifier] $ do
-            route $ removeExtension
-            compile $ pandocCompiler
-                >>= saveSnapshot "content"
-                >>= loadAndApplyTemplate "templates/_post.html" (postWithTagsCtx tags)
-                >>= loadAndApplyTemplate "templates/default.html" (postWithTagsCtx tags)
-                >>= relativizeUrls
+    match "posts/*" $ do
+        route $ removeExtension
+        compile $ pandocCompiler
+            >>= saveSnapshot "content"
+            >>= loadAndApplyTemplate "templates/_post.html" (postWithTagsCtx tags)
+            >>= loadAndApplyTemplate "templates/default.html" (postWithTagsCtx tags)
+
+    -- Tags pages
 
     create ["tags/index.html"] $ do
         route idRoute
@@ -54,11 +50,11 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/default.html" postCtx
 
     tagsRules tags $ \tag identifiers -> do
-        paginate <- buildPaginateWith 5 (getTagIdent tag) identifiers
+        paginate <- buildPaginateWith' 5 (getTagIdent tag) identifiers
         paginateRules paginate $ \page ids -> do
             route idRoute
             compile $ do
-                posts <- loadAllSnapshots ids "content"
+                posts <- recentFirst =<< loadAllSnapshots ids "content"
                 let postsCtx =
                         listField "posts" postCtx (return posts) `mappend`
                         constField "navlinkolder" "" `mappend`
@@ -81,13 +77,13 @@ main = hakyll $ do
             makeItem ""
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
 
 
+    {-
     match "index.md" $ do
         route $ setExtension "html"
         compile $ do
-            posts <- fmap (take 5) . recentFirst =<< loadAllSnapshots "posts/*" "content"
+            posts <- fmap (take 5) . recentFirst =<< loadAll ("posts/*" .&&. hasVersion "list")
 
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
@@ -96,9 +92,25 @@ main = hakyll $ do
 
             pandocCompiler
                 >>= loadAndApplyTemplate "templates/_post-without-footer.html" postCtx
-                >>= loadAndApplyTemplate "templates/index.html" indexCtx
-                >>= relativizeUrls
+                >>= loadAndApplyTemplate "templates/index.html" indexCtx -}
 
+    paginate <- buildPaginateWith' 5 getPageIdent ("posts/*")
+    paginateRules paginate $ \page ids -> do
+        create [paginateMakeId paginate page] $ do
+            route idRoute
+            compile $ do
+                posts <- recentFirst =<< loadAllSnapshots ids "content"
+                let postsCtx =
+                        listField "posts" postCtx (return posts) `mappend`
+                        constField "navlinkolder" "" `mappend`
+                        constField "navlinknewer" "" `mappend`
+                        defaultContext
+
+                makeItem ""
+                    >>= loadAndApplyTemplate "templates/list.html" postsCtx
+
+
+    {-
     paginate 5 $ \index maxIndex itemsForPage -> do
         let id = fromFilePath $ "page/" ++ (show index) ++ "/index.html"
         if index == 1 then return ()
@@ -116,6 +128,7 @@ main = hakyll $ do
 
                     makeItem ""
                         >>= loadAndApplyTemplate "templates/list.html" postsCtx
+                        -}
 
     match (fromList ["about.md", "shoutbox.md"]) $ do
         route $ removeExtension
@@ -127,10 +140,16 @@ main = hakyll $ do
 
 
 --------------------------------------------------------------------------------
+
 getTagIdent :: String -> PageNumber -> Identifier
 getTagIdent tag pageNum
     | pageNum == 1 = fromFilePath $ "tag/" ++ tag ++ "/index.html"
     | otherwise = fromFilePath $ "tag/" ++ tag ++ "/page/" ++ (show pageNum) ++ "/index.html"
+
+getPageIdent :: PageNumber -> Identifier
+getPageIdent pageNum
+    | pageNum == 1 = fromFilePath $ "index.html"
+    | otherwise = fromFilePath $ "page/" ++ (show pageNum) ++ "/index.html"
 
 postWithTagsCtx :: Tags -> Context String
 postWithTagsCtx tags = tagsField "tags" tags `mappend`
@@ -139,6 +158,7 @@ postWithTagsCtx tags = tagsField "tags" tags `mappend`
 postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
+    field "url" (return . identifierToUrl . toFilePath . itemIdentifier) `mappend`
     defaultContext
 
 getPrevNavLink :: Int -> Int -> String
@@ -152,37 +172,6 @@ getNextNavLink index maxIndex
     | index == 2 = "<li class=\"next\"><a href=\"/\">Моложе &rarr;</a></li>"
     | otherwise = "<li class=\"next\"><a href=\"/page/" ++ (show $ index - 1) ++ "/\">Моложе &rarr;</a></li>"
 
---------------------------------------------------------------------------------
--- | Split list into equal sized sublists.
--- https://github.com/ian-ross/blog
-chunk :: Int -> [a] -> [[a]]
-chunk n [] = []
-chunk n xs = ys : chunk n zs
-    where (ys,zs) = splitAt n xs
-
-paginate:: Int -> (Int -> Int -> [Identifier] -> Rules ()) -> Rules ()
-paginate itemsPerPage rules = do
-    identifiers <- getMatches "posts/*"
-    items <- filterM isPublished identifiers
-    let sorted = reverse $ sortBy byDate items
-        chunks = chunk itemsPerPage sorted
-        maxIndex = length chunks
-        pageNumbers = take maxIndex [1..]
-        process i is = rules i maxIndex is
-    zipWithM_ process pageNumbers chunks
-        where
-            byDate id1 id2 =
-                let fn1 = takeFileName $ toFilePath id1
-                    fn2 = takeFileName $ toFilePath id2
-                    parseTime' fn = parseTime defaultTimeLocale "%Y-%m-%d" $ intercalate "-" $ take 3 $ splitAll "-" fn
-                in compare ((parseTime' fn1) :: Maybe UTCTime) ((parseTime' fn2) :: Maybe UTCTime)
-
-matchPosts :: (Identifier -> Rules ()) -> Rules ()
-matchPosts process = do
-    identifiers <- getMatches "posts/*"
-    items <- filterM isPublished identifiers
-    forM_ items process
-
 isPublished :: (MonadMetadata m) => Identifier -> m Bool
 isPublished identifier = do
     published <- getMetadataField identifier "published"
@@ -191,12 +180,17 @@ isPublished identifier = do
 -- | Transforms 'something/something.md' into 'something/something/index.html'
 -- and 'something/YYYY-MM-DD-something.md' into 'something/something/index.html'
 removeExtension :: Routes
-removeExtension = customRoute $
-    (\filepath ->
-        subRegex (mkRegex "^(.*)\\.md$")
-            (subRegex (mkRegex "/[0-9]{4}-[0-9]{2}-[0-9]{2}-(.*)\\.md$") filepath "/\\1/index.html")
-            "\\1/index.html") .
-    toFilePath
+removeExtension = customRoute $ removeExtension' . toFilePath
+
+removeExtension' :: String -> String
+removeExtension' filepath = subRegex (mkRegex "^(.*)\\.md$")
+                                        (subRegex (mkRegex "/[0-9]{4}-[0-9]{2}-[0-9]{2}-(.*)\\.md$") filepath "/\\1/index.html")
+                                        "\\1/index.html"
+
+identifierToUrl :: String -> String
+identifierToUrl filepath = subRegex (mkRegex "^(.*)\\.md$")
+                                        (subRegex (mkRegex "/[0-9]{4}-[0-9]{2}-[0-9]{2}-(.*)\\.md$") filepath "/\\1/")
+                                        "\\1/"
 
 countText :: Int -> String -> String -> String -> String
 countText count one two many
@@ -214,3 +208,33 @@ getWeight minCount maxCount count =
     round ((5 * ((fromIntegral count :: Double) - fromIntegral minCount) +
         fromIntegral maxCount - fromIntegral minCount) /
         (fromIntegral maxCount - fromIntegral minCount))
+
+
+buildPaginateWith' :: MonadMetadata m
+                  => Int
+                  -> (PageNumber -> Identifier)
+                  -> Pattern
+                  -> m Paginate
+buildPaginateWith' n makeId pattern = do
+    -- TODO filter unpublished
+
+    metadata <- getAllMetadata pattern
+    let idents         = fst $ unzip $ sortBy compareFn metadata
+        pages          = flip unfoldr idents $ \xs ->
+            if null xs then Nothing else Just (splitAt n xs)
+        nPages         = length pages
+        paginatePages' = zip [1..] pages
+        pagPlaces'     =
+            [(ident, idx) | (idx,ids) <- paginatePages', ident <- ids] ++
+            [(makeId i, i) | i <- [1 .. nPages]]
+
+    return $ Paginate (M.fromList paginatePages') (M.fromList pagPlaces') makeId
+        (PatternDependency pattern idents)
+
+    where
+        compareFn :: (a, Metadata) -> (a, Metadata) -> Ordering
+        compareFn (_, a) (_, b)
+            | M.lookup "date" a == Nothing && M.lookup "date" b == Nothing = EQ
+            | M.lookup "date" a == Nothing = GT
+            | M.lookup "date" b == Nothing = LT
+            | otherwise = compare (b M.! "date") (a M.! "date")
